24f724b246e037d91d85fab55026e4263f41f748
incapable <5178501+wohaohaohaohao@user.noreply.gitee.com>
2021-11-30 19:29:13 +0800

    create and testSuccess

---
+++ src/main/java/com/finaldevelopment/YH/FinalDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.finaldevelopment.YH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalDevelopmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalDevelopmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/finaldevelopment/YH/FinalDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finaldevelopment.YH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalDevelopmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
